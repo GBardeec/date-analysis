3066fc4c4b1164011042fe9d38f8c916415acdd6
Ильяc <ilyas@MacBook-Pro-Ilac.local>
2025-03-14 16:19:43 +0400

    Initial commit

---
+++ app/Console/Commands/DateAnalysisCommand.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class DateAnalysisCommand extends Command
+{
+    protected $signature = 'app:date-analysis-command';
+
+    protected $description = 'Command description';
+
+    public function handle()
+    {
+        //
+    }
+}
